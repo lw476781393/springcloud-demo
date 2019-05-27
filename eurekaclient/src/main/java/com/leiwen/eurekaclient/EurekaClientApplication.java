b5a29a195e84ff3a0d69bb3a82bcac3ad76b9ea2
Leiwen <476781393>
2019-05-27 22:17:23 +0800

    初始

---
+++ eurekaclient/src/main/java/com/leiwen/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,11 @@
+package com.leiwen.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
